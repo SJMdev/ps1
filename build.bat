319a5ba264fd24b2964439e7ea05274bc9f8bac8
ere erer <sjorsmallon@gmail.com>
2024-05-20 16:23:11 +0200

    [fix] build files

--- build.bat
+++ build.bat
@@ -2,3 +2,5 @@ armips fillmem.s -root src
 armips exercise1.s -root src
 armips exercise2.s -root src
 armips exercise3.s -root src
+armips factorial.s -root src
+
--- generate_exe.bat
+++ generate_exe.bat
@@ -2,4 +2,6 @@ python helper_scripts/bin2exe.py src/fillmem.bin bld/fillmem.ps-exe
 python helper_scripts/bin2exe.py src/exercise1.bin bld/exercise1.ps-exe
 python helper_scripts/bin2exe.py src/exercise2.bin bld/exercise2.ps-exe
 python helper_scripts/bin2exe.py src/exercise3.bin bld/exercise3.ps-exe
+python helper_scripts/bin2exe.py src/factorial.bin bld/factorial.ps-exe
+
 
